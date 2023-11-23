18d779ee3825dcb3de8924dc870ca14e490e6f9d
anggagumilang212 <anggagumilang212@gmail.com>
2023-11-24 06:33:09 +0700

    api get author

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\AuthorResource;
+use App\Models\Author;
 
 class AuthorController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $author = Author::all();
+        return AuthorResource::collection($author)->additional(['message' => 'Data authors successfully']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,9 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//project
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{id}', [ProjectController::class, 'detail']);
 Route::post('/create-projects', [ProjectController::class, 'store']);
 Route::put('/update-projects/{id}', [ProjectController::class, 'update']);
 Route::delete('/delete-projects/{id}', [ProjectController::class, 'destroy']);
+
+//auhtor
+Route::get('/authors', [AuthorController::class, 'index']);
