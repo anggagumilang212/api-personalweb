2e2afdfc9ea8f0ffa155bbac3fac2e58f227f968
anggagumilang212 <anggagumilang212@gmail.com>
2026-09-04 14:13:10 +0700

    refactor: update project and blog routes to use PUT instead of POST for resource updates

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,13 @@ Route::get('/', function () {
 // Projects
 Route::get('/projects', [ProjectViewController::class, 'index'])->name('projects.index');
 Route::post('/projects', [ProjectViewController::class, 'store'])->name('projects.store');
-Route::post('/projects/{id}', [ProjectViewController::class, 'update'])->name('projects.update');
+Route::put('/projects/{id}', [ProjectViewController::class, 'update'])->name('projects.update');
 Route::delete('/projects/{id}', [ProjectViewController::class, 'destroy'])->name('projects.destroy');
 
 // Blogs
 Route::get('/blogs', [BlogViewController::class, 'index'])->name('blogs.index');
 Route::post('/blogs', [BlogViewController::class, 'store'])->name('blogs.store');
-Route::post('/blogs/{id}', [BlogViewController::class, 'update'])->name('blogs.update');
+Route::put('/blogs/{id}', [BlogViewController::class, 'update'])->name('blogs.update');
 Route::delete('/blogs/{id}', [BlogViewController::class, 'destroy'])->name('blogs.destroy');
 
 // Resumes / CV
